4b00afcc0448917b09c0a3990a86231fb5d4ffbe
Henrique Silva <hbs003@hotmail.com>
2018-07-17 14:35:11 -0300

    admin done

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,14 @@
 Rails.application.routes.draw do
-  get 'subscribers/new'
+
   devise_for :users, controllers: { registrations: "registrations"}
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :bookings, only:[:new, :create, :show, :edit, :update] do
-    member do
-      get 'traveller' , to: "bookings#traveller"
-      get 'confirmation', to: "bookings#confirmation", as: "confirmation"
-    end
-  end
-
 #maybe payment needs to be nested above
-  resources :bookings, only: [:show, :create] do
-    resources :payments, only: [:new, :create]
-  end
+  get "/adm-painel", to: "pages#adm_painel", as: "adm_painel"
 
   get "/coming-soon", to: "pages#coming_soon", as: "coming_soon"
-  resources :subscribers, only: [:create, :new]
+  resources :subscribers, only: [:create, :new, :index]
 
   resources :posts
 
