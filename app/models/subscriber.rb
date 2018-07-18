52741b7b349215eb524417ad61bf6566c5f8dd9e
Henrique Silva <hbs003@hotmail.com>
2018-07-18 10:31:44 -0300

    mailchimp

--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -2,4 +2,11 @@ class Subscriber < ApplicationRecord
   validates :email, presence: true
   validates :email, uniqueness: true
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
+  after_create :subscribe_to_newsletter
+
+  private
+
+  def subscribe_to_newsletter
+    SubscribeToNewsletterService.new(self).call
+  end
 end
