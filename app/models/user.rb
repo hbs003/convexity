04a33e9bb36c7b9df9fa0f616b1db8ce143dbe8c
Henrique Silva <hbs003@hotmail.com>
2018-08-27 16:21:45 -0300

    sssss

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,20 +4,26 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_many :stocks
   has_many :enterprises, through: :stocks
   has_many :reviews, dependent: :destroy
-  has_many :stocks
   has_many :investments
   validates :email, presence: true
   validates :email, uniqueness: true
   validates :cpf, uniqueness: true
   after_create :subscribe_to_newsletter
   mount_uploader :photo, PhotoUploader
+
+
+
+
   private
 
   def subscribe_to_newsletter
     SubscribeToNewsletterService.new(self).call
   end
+
+
 
 
 
