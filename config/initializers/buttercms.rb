b42efea4c68650f8c433e12d868014c8742b2f5f
Henrique Silva <hbs003@hotmail.com>
2018-10-16 00:30:47 -0300

    new blog

--- config/initializers/buttercms.rb
+++ config/initializers/buttercms.rb
@@ -1,8 +1,8 @@
-require 'buttercms-ruby'
+# require 'buttercms-ruby'
 
-# If you added the Heroku Butter add-on, ENV["BUTTER_TOKEN"] will be defined.
-# Otherwise, grab your token at https://buttercms.com/profile/ and set it below
-ButterCMS::api_token = ENV['BUTTER_TOKEN']
+# # If you added the Heroku Butter add-on, ENV["BUTTER_TOKEN"] will be defined.
+# # Otherwise, grab your token at https://buttercms.com/profile/ and set it below
+# ButterCMS::api_token = ENV['BUTTER_TOKEN']
 
-posts = ButterCMS::Post.search("my favorite post", {page: 1, page_size: 10})
-categories = ButterCMS::Category.all()
+# posts = ButterCMS::Post.search("my favorite post", {page: 1, page_size: 10})
+# categories = ButterCMS::Category.all()
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,7 @@
 Rails.application.routes.draw do
 
-  scope :module => 'buttercms' do
-    get '/categories/:slug' => 'categories#show', :as => :buttercms_category
-    get '/author/:slug' => 'authors#show', :as => :buttercms_author
+  mount Lines::Engine => "/blog"
 
-    get '/blog/rss' => 'feeds#rss', :format => 'rss', :as => :buttercms_blog_rss
-    get '/blog/atom' => 'feeds#atom', :format => 'atom', :as => :buttercms_blog_atom
-    get '/blog/sitemap.xml' => 'feeds#sitemap', :format => 'xml', :as => :buttercms_blog_sitemap
-
-    get '/blog(/page/:page)' => 'posts#index', :defaults => {:page => 1}, :as => :buttercms_blog
-    get '/blog/:slug' => 'posts#show', :as => :buttercms_post
-
-
-  end
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users, controllers: { registrations: "registrations"}
   root to: 'pages#countdown'
